99d51ff23d1059f3abf43b3b146eab3a12c883ac
anon <hisidblack@kakao.com>
2020-04-10 00:24:07 +0900

    Mode Setting

--- paintJS/app.js
+++ paintJS/app.js
@@ -2,20 +2,25 @@ const canvas = document.getElementById("jsCanvas");
 const ctx = canvas.getContext("2d");
 const colors = document.getElementsByClassName("controls_color");
 const range = document.getElementById("jsRange");
+const mode = document.getElementById("jsMode");
+const SIZE = 700;
 
 var painting = false;
+var filling = false;
 
-canvas.width = 700;
-canvas.height = 700;
+canvas.width = canvas.height = SIZE;
 
 ctx.strokeStyle = "#282828";
-ctx.lineWidth = 2.5;
+ctx.fillStyle = "#282828";
+ctx.lineWidth = 3.0;
+
 
 if(canvas){
   canvas.addEventListener("mousemove",onMouseMove);
-  canvas.addEventListener("mousedown",startPainting);
-  canvas.addEventListener("mouseup",stopPainting);
-  canvas.addEventListener("mouseleave",stopPainting);
+  canvas.addEventListener("mousedown",() => painting = true);
+  canvas.addEventListener("mouseup",() => painting = false);
+  canvas.addEventListener("mouseleave",() => painting = false);
+  canvas.addEventListener("click",fillPainting);
 }
 
 Array.from(colors).forEach(color => color.addEventListener("click",handleColor));
@@ -24,12 +29,14 @@ if(range){
   range.addEventListener("input",handleRange);
 }
 
-function startPainting(){
-  painting = true;
+if(mode){
+  mode.addEventListener("click",handleMode);
 }
 
-function stopPainting(){
-  painting = false;
+function fillPainting(){
+  if(filling){
+    ctx.fillRect(0,0,SIZE,SIZE);
+  }
 }
 
 function onMouseMove(event){
@@ -39,15 +46,28 @@ function onMouseMove(event){
     ctx.beginPath();
     ctx.moveTo(x,y);
   }else{
-    ctx.lineTo(x,y);
-    ctx.stroke();
+    if(!filling){
+      ctx.lineTo(x,y);
+      ctx.stroke();
+    }
   }
 }
 
 function handleColor(event){
-  ctx.strokeStyle = event.target.style.backgroundColor;
+  const color = event.target.style.backgroundColor;
+  ctx.strokeStyle = color;
+  ctx.fillStyle = color;
 }
 
 function handleRange(event){
   ctx.lineWidth = event.target.value;
+}
+
+function handleMode(event){
+  if(filling == true){
+    mode.innerText = "fill";
+  } else{
+    mode.innerText = "paint";
+  }
+  filling = filling ^ 1;
 }
